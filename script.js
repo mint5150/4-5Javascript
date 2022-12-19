e0b3384735738c19938f0423923a0e79a61fb57b
mint5150 <musia.home@gmail.com>
2022-12-19 20:43:22 +0900

    master

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 /*3. 数値型を比較してelseの処理に入る処理を作ってください。*/
 var num = 10;
 if(num === 1){
-console.log("trueの処理");
-console.log(num);
-} else {
-console.log("falseの処理");
-console.log(num);
+    console.log("trueの処理");
+    console.log(num);
+  } else {
+    console.log("falseの処理");
+    console.log(num);
 };
 
 /*4. 文字列型を比較してelseの処理に入る処理を作ってください。*/
 var text = "JavaScript";
 if(text === "js"){
-console.log("trueの処理");
-console.log(num);
-} else {
-console.log("falseの処理");
-console.log(text);
+    console.log("trueの処理");
+    console.log(num);
+  } else {
+    console.log("falseの処理");
+    console.log(text);
 };
